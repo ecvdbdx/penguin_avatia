2edc327d2ee2593486f040d87fd5f7a9a3878bbc
Mathieu Daix <mathieudaixpro@gmail.com>
2019-12-05 15:28:16 +0100

    first devs with cra

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import axios from 'axios'
+import Chart from './components/Chart'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	constructor() {
+		super()
+		this.state = {
+			cryptos: []
+		}
+	}
+	componentDidMount() {
+		axios.get('https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD,JPY,EUR')
+			.then(res => {
+				const cryptos = res.data;
+				// console.log(cryptos)
+				this.setState({ cryptos: cryptos })
+			})
+	}
+	render() {
+		console.log('state', this.state)
+
+		return (
+			<div className="App">
+				{Object.keys(this.state.cryptos).map((key, index) => (
+					<div key={index}>
+						<span>{key}</span>
+						<span>{this.state.cryptos[key]}</span>
+					</div>
+				))}
+				<Chart data={this.state.cryptos.USD} />
+			</div>
+		);
+	}
 }
 
 export default App;
