b7cf9c6be6cab375ad7b5dfa47857d0e4eeea57a
robingiraud <robin.giraud5@gmail.com>
2019-12-05 17:27:22 +0100

    :sparkles: Introducing Chartjs2

--- src/App.js
+++ src/App.js
@@ -7,28 +7,31 @@ class App extends Component {
     constructor() {
         super();
         this.state = {
-            cryptos: []
+            cryptos: undefined
         };
     }
     componentDidMount() {
-        axios.get("https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD,JPY,EUR").then(res => {
+        axios.get("https://min-api.cryptocompare.com/data/v2/histominute?fsym=BTC&tsym=EUR&limit=10").then(res => {
             const cryptos = res.data;
-            // console.log(cryptos)
             this.setState({ cryptos: cryptos });
         });
     }
     render() {
-        console.log("state", this.state);
+        // console.log("state", this.state.cryptos && this.state.cryptos.Data.Data);
+
+        const values = this.state.cryptos && this.state.cryptos.Data.Data.map(o =>o.open)
+        /* {Object.keys(this.state.cryptos.Data.Data).map((key, index) => (
+                            <div key={index}>
+                                <span>{key}</span>
+                                <span>{this.state.cryptos[key]}</span>
+                            </div>
+                        ))}
+                        <Chart data={this.state.cryptos.USD} /> */
 
         return (
             <div className="App">
-                {Object.keys(this.state.cryptos).map((key, index) => (
-                    <div key={index}>
-                        <span>{key}</span>
-                        <span>{this.state.cryptos[key]}</span>
-                    </div>
-                ))}
-                <Chart data={this.state.cryptos.USD} />
+                <p>toto</p>
+                <Chart data={values} />
             </div>
         );
     }
