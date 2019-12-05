23b638b26651127de0298c1f510cfe1b1a8acb9d
Mathieu Daix <mathieudaixpro@gmail.com>
2019-12-05 15:59:37 +0100

    add scss structure

--- src/App.js
+++ src/App.js
@@ -1,38 +1,37 @@
-import React, { Component } from 'react'
-import './App.css'
-import axios from 'axios'
-import Chart from './components/Chart'
+import React, { Component } from "react";
+import "./scss/components/App.scss";
+import axios from "axios";
+import Chart from "./components/Chart";
 
 class App extends Component {
-	constructor() {
-		super()
-		this.state = {
-			cryptos: []
-		}
-	}
-	componentDidMount() {
-		axios.get('https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD,JPY,EUR')
-			.then(res => {
-				const cryptos = res.data;
-				// console.log(cryptos)
-				this.setState({ cryptos: cryptos })
-			})
-	}
-	render() {
-		console.log('state', this.state)
+    constructor() {
+        super();
+        this.state = {
+            cryptos: []
+        };
+    }
+    componentDidMount() {
+        axios.get("https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD,JPY,EUR").then(res => {
+            const cryptos = res.data;
+            // console.log(cryptos)
+            this.setState({ cryptos: cryptos });
+        });
+    }
+    render() {
+        console.log("state", this.state);
 
-		return (
-			<div className="App">
-				{Object.keys(this.state.cryptos).map((key, index) => (
-					<div key={index}>
-						<span>{key}</span>
-						<span>{this.state.cryptos[key]}</span>
-					</div>
-				))}
-				<Chart data={this.state.cryptos.USD} />
-			</div>
-		);
-	}
+        return (
+            <div className="App">
+                {Object.keys(this.state.cryptos).map((key, index) => (
+                    <div key={index}>
+                        <span>{key}</span>
+                        <span>{this.state.cryptos[key]}</span>
+                    </div>
+                ))}
+                <Chart data={this.state.cryptos.USD} />
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,36 +1,20 @@
-import React, { Component } from 'react';
-import { Bar, Line, Pie } from 'react-chartjs-2';
+import React, { Component } from "react";
+import { Line } from "react-chartjs-2";
 
 class Chart extends Component {
     constructor(props) {
-        super(props)
+        super(props);
         this.state = {
             chartData: {
-                // labels: ,
                 datasets: [
                     {
-                        label: 'Population',
-                        data: [
-                            617594,
-                            181045,
-                            153060,
-                            106519,
-                            105162,
-                            95072
-                        ],
-                        backgroundColor: [
-                            'rgba(255, 99, 132, 0.6)',
-                            'rgba(54, 162, 235, 0.6)',
-                            'rgba(255, 206, 86, 0.6)',
-                            'rgba(75, 192, 192, 0.6)',
-                            'rgba(153, 102, 255, 0.6)',
-                            'rgba(255, 159, 64, 0.6)',
-                            'rgba(255, 99, 132, 0.6)'
-                        ]
+                        label: "Population",
+                        data: [617594, 181045, 153060, 106519, 105162, 95072],
+                        backgroundColor: ["rgba(255, 99, 132, 0.6)", "rgba(54, 162, 235, 0.6)", "rgba(255, 206, 86, 0.6)", "rgba(75, 192, 192, 0.6)", "rgba(153, 102, 255, 0.6)", "rgba(255, 159, 64, 0.6)", "rgba(255, 99, 132, 0.6)"]
                     }
                 ]
             }
-        }
+        };
     }
 
     render() {
@@ -39,7 +23,7 @@ class Chart extends Component {
                 <Line data={this.state.chartData} options={{ maintainAspectRatio: false }} />
                 <div>{this.props.data}</div>
             </div>
-        )
+        );
     }
 }
 
