d13c14ac6c12360ed8d138c9de2fd23e7b9c9e82
Mathieu Daix <mathieudaixpro@gmail.com>
2019-12-12 13:07:58 +0100

    change style of graph

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
-import React, { Component } from "react";
-import "./scss/components/App.scss";
-import axios from "axios";
-import Chart from "./components/Chart";
+import React, { Component } from 'react'
+import './scss/components/App.scss'
+import axios from 'axios'
+import Chart from './components/Chart'
 
 class App extends Component {
-    constructor() {
-        super();
-        this.state = {
-            cryptos: undefined
-        };
-    }
-    componentDidMount() {
-        axios.get("https://min-api.cryptocompare.com/data/v2/histominute?fsym=BTC&tsym=EUR&limit=60").then(res => {
-            const cryptos = res.data;
-            this.setState({ cryptos: cryptos });
-        });
-    }
-    render() {
-        const values = this.state.cryptos ? this.state.cryptos.Data.Data.map(o => o.open) : [];
-        const time = this.state.cryptos ? this.state.cryptos.Data.Data.map(o => o.time) : [];
+	constructor() {
+		super()
+		this.state = {
+			cryptos: undefined
+		}
+	}
+	componentDidMount() {
+		axios.get('https://min-api.cryptocompare.com/data/v2/histominute?fsym=BTC&tsym=EUR&limit=60').then(res => {
+			const cryptos = res.data
+			this.setState({ cryptos: cryptos })
+		})
+	}
+	render() {
+		const values = this.state.cryptos ? this.state.cryptos.Data.Data.map(o => o.open) : []
+		const time = this.state.cryptos ? this.state.cryptos.Data.Data.map(o => o.time) : []
 
-        return (
-            <div className="App">
-                <Chart data={values} time={time} />
-            </div>
-        );
-    }
+		return (
+			<div className="App">
+				<Chart data={values} time={time} />
+			</div>
+		)
+	}
 }
 
-export default App;
+export default App
