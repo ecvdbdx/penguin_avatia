85ca2eaacff603b2daac6c16017d86738a932cc5
robingiraud <robin.giraud5@gmail.com>
2019-12-12 16:52:49 +0100

    :art: Structure improvements : componentDidUpdate + createUrl

--- src/App.js
+++ src/App.js
@@ -3,51 +3,57 @@ import './scss/components/App.scss'
 import axios from 'axios'
 import Chart from './components/Chart'
 
+function createUrl(histo, limit) {
+	return (`https://min-api.cryptocompare.com/data/v2/histo${histo}?fsym=BTC&tsym=EUR&limit=${limit}`);
+}
+
 class App extends Component {
 	constructor() {
 		super()
 		this.state = {
 			cryptos: undefined,
-			selected: 'day'
+			selected: ''
 		}
 	}
 	updateScale(scale) {
 		this.setState({ selected: scale });
-		let histo = '';
-		let limit = '';
-		switch (scale) {
-			case 'hour':
-				histo = 'minute';
-				limit = 60;
-				break;
-			case 'day':
-				histo = 'minute';
-				limit = 1440;
-				break;
-			case 'week':
-				histo = 'hour';
-				limit = 1260;
-				break;
-			case 'month':
-				histo = 'day';
-				limit = 30;
-				break;
-			case 'year':
-				histo = 'day';
-				limit = 365;
-				break;
-			default:
-				histo = 'minute';
-				limit = 1440;
-		}
-		axios.get(`https://min-api.cryptocompare.com/data/v2/histo${histo}?fsym=BTC&tsym=EUR&limit=${limit}`).then(res => {
-			this.setState({ cryptos: undefined });
-			this.setState({ cryptos: res.data });
-		})
 	}
-
-	componentDidUpdate() {
+	componentDidUpdate(_, prevState) {
 		// Tester si state.selected différent de celui d'avant, si oui on update avec le switch et requete...
+		if (prevState.selected !== this.state.selected) {
+			console.log('coucou', prevState.selected, this.state.selected)
+			let histo = '';
+			let limit = '';
+			switch (this.state.selected) {
+				case 'hour':
+					histo = 'minute';
+					limit = 60;
+					break;
+				case 'day':
+					histo = 'minute';
+					limit = 1440;
+					break;
+				case 'week':
+					histo = 'hour';
+					limit = 1260;
+					break;
+				case 'month':
+					histo = 'day';
+					limit = 30;
+					break;
+				case 'year':
+					histo = 'day';
+					limit = 365;
+					break;
+				default:
+					histo = 'minute';
+					limit = 1440;
+			}
+			axios.get(createUrl(histo, limit)).then(res => {
+				this.setState({ cryptos: undefined });
+				this.setState({ cryptos: res.data });
+			})
+		}
 	}
 
 	componentDidMount() {
